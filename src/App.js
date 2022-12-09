e091f98a8189abbe4284d3ac21373755c652202e
Sheriffdeen Yusuf <sheriffdeenyusuf1130@gmail.com>
2022-12-09 21:59:16 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+        <h1>Testing react</h1>
+        <h3>setting up</h3>
+    </div>
+  );
+}
+
+export default App;
